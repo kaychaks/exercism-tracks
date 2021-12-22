65bc99ebc6729a66277cb8612cea32f4d103b4f0
Kaushik Chakraborty <git@kaushikc.org>
2021-12-22 20:21:40 +0530

    clock - iteration 2

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,80 +1,21 @@
 use std::fmt::Display;
 
-#[derive(Copy, Clone, PartialEq, Debug)]
-pub struct Clock(i32, i32);
+#[derive(PartialEq, Eq, Debug)]
+pub struct Clock(i32);
 
 impl Display for Clock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
-        write!(f, "{:02}:{:02}", self.0, self.1)
+        write!(f, "{:02}:{:02}", self.0 / 60, self.0 % 60)
     }
 }
 
-fn adjust_hour(hrs: i32) -> i32 {
-    match hrs.abs() {
-        24 => 0,
-        0..=23 => hrs,
-        _ => hrs % 24,
-    }
-}
-
-fn adjust_min(mins: i32) -> (i32, i32) {
-    match mins.abs() {
-        60 => (mins / 60, 0),
-        0..=59 => (0, mins),
-        _ => (adjust_hour(mins / 60), mins % 60),
-    }
-}
-
-fn check_hr_neg(hr: i32) -> i32 {
-    if hr.is_negative() {
-        24 - hr.abs()
-    } else {
-        hr
-    }
-}
-
-fn check_min_neg(min: i32, hr: i32) -> (i32, i32) {
-    if min.is_negative() {
-        let h = check_hr_neg(adjust_hour(hr - 1));
-        let m = 60 - min.abs();
-        (h, m)
-    } else {
-        (hr, min)
-    }
-}
-
-fn proc(hr: i32, min: i32) -> (i32, i32) {
-    let (mut h, m) = adjust_min(min);
-    h = check_hr_neg(adjust_hour(hr + h));
-
-    check_min_neg(m, h)
-}
-
+const DAY: i32 = 24 * 60;
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut x = Clock(0, 0);
-
-        x.0 = check_hr_neg(adjust_hour(hours));
-
-        let (a, b) = proc(x.0, minutes);
-
-        x.0 = a;
-        x.1 = b;
-        x
+        Clock((hours * 60 + minutes).rem_euclid(DAY))
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let mut x = *self;
-
-        let (h, m) = adjust_min(minutes);
-
-        x.0 += h;
-        x.1 += m;
-
-        let (a, b) = proc(x.0, x.1);
-
-        x.0 = a;
-        x.1 = b;
-        x
+        Clock((self.0 + minutes).rem_euclid(DAY))
     }
 }
